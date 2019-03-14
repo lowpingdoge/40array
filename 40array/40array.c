aec01b6bebeb7e18fbf4f195ad336aa14cc8ae85
Dan Li <akiraryo@hotmail.com>
2019-03-14 20:05:30 +0100

    折半查找实现完成。

--- 40array/40array.c
+++ 40array/40array.c
@@ -5,7 +5,7 @@
 
 int main() {
 	int arr[10]={1,2,3,4,5,6,7,8,9,10};
-	int findData = -1;
+	int findData = 10;
 	int left = 0, right = 9, midIdx;
 	int idx = -1;
 
@@ -18,9 +18,14 @@ int main() {
 			break;
 		}
 		else if (findData > arr[midIdx])
-		{
-
-		}
+			left = midIdx + 1;
+		else
+			right = midIdx - 1;
 	}
+	if(idx == -1)
+		printf("find none\n");
+	else
+		printf("idx = %d\n",midIdx);
 
+	return 0;
 }
