369f3bde7b6377e0eda38eb04f28d6797e830f99
Dan Li <akiraryo@hotmail.com>
2019-03-14 19:33:13 +0100

    折半查找实现

--- 40array/40array.c
+++ 40array/40array.c
@@ -1,7 +1,26 @@
+﻿//折半查找实现
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
+	int arr[10]={1,2,3,4,5,6,7,8,9,10};
+	int findData = -1;
+	int left = 0, right = 9, midIdx;
+	int idx = -1;
+
+	while (left <= right)
+	{
+		midIdx = (left + right) / 2;
+		if (findData == arr[midIdx])
+		{
+			idx = midIdx;
+			break;
+		}
+		else if (findData > arr[midIdx])
+		{
+
+		}
+	}
 
 }
